21ea391a1a27a1ca65fc84b94a31ebe203ea5104
lucafluri <me@lucafluri.ch>
2022-12-04 14:42:23 +0100

    04 done

--- 04/part1.py
+++ 04/part1.py
@@ -1,2 +1,33 @@
 file1 = open('input.txt', 'r')
 lines = file1.readlines()
+
+sum = 0
+
+for line in lines:
+    seqs =  line.split(',')
+    s1 = seqs[0].strip()
+    s2 = seqs[1].strip()
+    
+    a, b = s1.split('-')
+    c, d = s2.split('-')
+    
+    # COnvert to int!!
+    a = int(a)
+    b = int(b)
+    c = int(c)
+    d = int(d)
+    
+    # Case 1: a >= c and b <= d
+    # Case 2: c >= a and d <= b
+    
+    # A-B, C-D
+    
+    if(a >= c and b <= d): # S1 in S2
+        sum+=1
+        continue
+    if(a <= c and b >= d): # S2 in S1
+        sum+=1
+    
+print(sum)
+    
+    
