0705545106db6866726f9a7b84ad5e32d662ca53
Luca Fluri <luca.fluri@fhnw.ch>
2022-12-08 10:35:18 +0100

    08 part 1 done

--- 08/part1.py
+++ 08/part1.py
@@ -7,26 +7,44 @@ lines = file.readlines()
 # Count visible trees
 
 def isVisible(x, y):
-    h = forest[x][y]
+    h = forest[y][x]
+    # print("Checking tree at " + str(x) + ", " + str(y) + " with height " + str(h))
     
-    count = 0
+    
+    count = [0, 0, 0, 0] # left, right, up, down
+    visible = False
     # Check left
     for i in range(x-1, -1, -1):
-        if forest[i][y] < h:
-            return True
+        if forest[y][i] < h:
+            count[0] += 1
+        else: 
+            visible = False
+            break
     # Check right
     for i in range(x+1, len(forest[0])):
-        if forest[i][y] < h:
-            return True
+        if forest[y][i] < h:
+            count[1] += 1
+        else: 
+            visible = False
+            break
     # Check up
     for i in range(y-1, -1, -1):
-        if forest[x][i] < h:
-            return True
+        # print(x, i, forest[i][x], h)
+        if forest[i][x] < h:
+            count[2] += 1
+        else: 
+            visible = False
+            break
     # Check down
     for i in range(y+1, len(forest)):
-        if forest[x][i] < h:
-            return True
-    return False
+        if forest[i][x] < h:
+            count[3] += 1
+        else: 
+            visible = False
+            break
+    # print(count, end=" ")
+    # print(max(count) > 0)
+    return (count[0] == x or count[1] == len(forest[0])-x-1 or count[2] == y or count[3] == len(forest)-y-1)
     
 
 sum = 0
@@ -40,8 +58,8 @@ sum += len(forest)*2 + len(forest[0])*2 - 4
 
 
 
-for x in range(len(forest[0])):
-    for y in range(len(forest)):
+for x in range(1, len(forest[0])-1):
+    for y in range(1, len(forest)-1):
         if(isVisible(x, y)):
             sum += 1
 
