2b75bc64eb7e82e099ae3280b6753ae1f3555212
Luca Fluri <me@lucafluri.ch>
2022-12-13 17:27:21 +0100

    progress 13

--- 13/part1.py
+++ 13/part1.py
@@ -17,22 +17,36 @@ def compare(l, r):
     print(l, r)
     # Both ints
     if(isinstance(l, int) and isinstance(r, int)):
+        print("Both ints")
         if(r < l): return False
+        if(r > l): return True
         
     
     # Both list
     if(isinstance(l, list) and isinstance(r, list)):
+        print("Both lists")
         if(len(l) >= len(r) ):
+            print("same length")
             for idx, val in enumerate(l):
-                compare(val, r[idx])
+                # print(val, r[idx]
+                ret = compare(val, r[idx])
+                if(ret): return True
+                # if(not compare(val, r[idx])): return False
+                # else: continue
         else:
             return False
     
     # One of each
     if(isinstance(r, list)):
+        print("One of each")
         return compare([l], r)
-    else:
+    elif(isinstance(l, list)):
         return compare(l, [r])
+    return False
+    
 
-for idx, pair in enumerate(pairs):
-    print("Pair: ", idx+1, compare(pair[0], pair[1]))
+c = 1
+print(compare(pairs[c][0], pairs[c][1]))
+# for idx, pair in enumerate(pairs):
+    # print(pair[0], pair[1])
+    # print("Result: ", compare(pair[0], pair[1]))
