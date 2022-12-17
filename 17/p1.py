2ce66307dd7732d72346022bbecece60debbba34
lucafluri <me@lucafluri.ch>
2022-12-17 13:23:57 +0100

    begin 17

---
+++ 17/p1.py
@@ -0,0 +1,49 @@
+file = open('17/in2.txt', 'r')
+line = file.readline()
+
+# 7 slots wide, basically automated tetris, input loops
+# Rocks appear, left edge at x=2 and bottom edge at 3 + highest rock/floor
+# All rocks (Loop in this order):
+# 
+# ####
+# 
+# .#.
+# ###
+# .#.
+# 
+# ..#
+# ..#
+# ###
+# 
+# #
+# #
+# #
+# #
+# 
+# ##
+# ##
+jets = []
+for c in line:
+    if c == '<':
+        jets.append(-1)
+    else: # c == '>'
+        jets.append(1)
+        
+print(jets)
+        
+# Highest rocks of each column
+hRocks = [0, 0, 0, 0, 0, 0, 0]
+maxH = max(hRocks)
+jetIndex = 0
+
+
+# Idea shortcut jet pushes until highest rock, then check in more detail
+start = maxH + 3
+x = 2
+jetSum = sum(jets[jetIndex:jetIndex+3])
+
+print(print(jetSum))
+    
+
+
+
